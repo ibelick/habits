9f533cabdd156c13ba0b85980ee791a1f2cf5edd
ibelick <julien.thibeaut@gmail.com>
2023-03-22 17:17:37 +0100

    Initial Commit

---
+++ habits/habitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  habitsApp.swift
+//  habits
+//
+//  Created by Julien THIBEAUT on 22/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct habitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
